7d8035fec17119c9602826436f078c9091d750f6
OmarHady7 <omarhady744@gmail.com>
2026-04-29 01:44:44 +0300

    Created basic aerodynamics simulator

---
+++ simulator.py
@@ -0,0 +1,22 @@
+from utils import calculate_drag, calculate_lift, get_cl_from_angle
+
+print("=== Aerodynamics Simulator ===")
+
+velocity = float(input("Velocity (m/s): "))
+wing_area = float(input("Wing Area: "))
+Cd = float(input("Drag Coefficient: "))
+angle = float(input("Angle of Attack (degrees): "))
+
+Cl = get_cl_from_angle(angle)
+
+drag = calculate_drag(velocity, wing_area, Cd)
+lift = calculate_lift(velocity, wing_area, Cl)
+
+print("\nResults:")
+print(f"Lift: {lift:.2f} N")
+print(f"Drag: {drag:.2f} N")
+
+if lift > drag:
+    print("Efficient Flight Condition")
+else:
+    print("Inefficient Design")
