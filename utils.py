e84b76899a8f4940ab863d1890f0ac44361499bc
OmarHady7 <omarhady744@gmail.com>
2026-04-30 02:05:29 +0300

    Improved lift model with stall limits

--- utils.py
+++ utils.py
@@ -7,7 +7,10 @@ def calculate_lift(v, A, Cl):
     return 0.5 * rho * v**2 * A * Cl
 
 def get_cl_from_angle(angle):
+    # Simple lift model: linear region + stall after 12 degrees
     if angle <= 12:
-        return 0.1 * angle
+        Cl = 0.1 * angle
     else:
-        return 1.2 - 0.1 * (angle - 12)
+        Cl = 1.2 - 0.1 * (angle - 12)
+    
+    return max(Cl, 0)
